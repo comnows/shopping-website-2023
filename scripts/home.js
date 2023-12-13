571455721cc5e8a046fec146ec90a37b09a987b3
comnows <watthanandh.l@mail.kmutt.ac.th>
2023-12-13 14:06:00 +0700

    Add slider dragging interaction

--- scripts/home.js
+++ scripts/home.js
@@ -5,6 +5,10 @@ const slideWidth = sliderContainer.querySelector('.slide').offsetWidth;
 const sliderPrevButton = sliderContainer.querySelector('.js-slider-button-prev');
 const sliderNextButton = sliderContainer.querySelector('.js-slider-button-next');
 
+let isDragging = false;
+let startX;
+let startScrollLeft;
+
 sliderPrevButton.addEventListener('click', () => {
     console.log(slideWidth);
     sliderWrapper.scrollLeft -= slideWidth;
@@ -14,3 +18,24 @@ sliderNextButton.addEventListener('click', () => {
     console.log(slideWidth);
     sliderWrapper.scrollLeft += slideWidth;
 });
+
+sliderContainer.addEventListener('mousedown', dragStart);
+sliderContainer.addEventListener('mousemove', sliderDragging);
+document.addEventListener('mouseup', dragStop);
+
+function dragStart(e) {
+    isDragging = true;
+    sliderWrapper.classList.add('dragging');
+    startX = e.pageX;
+    startScrollLeft = sliderWrapper.scrollLeft;
+}
+
+function sliderDragging(e) {
+    if(!isDragging) return;
+    sliderWrapper.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+function dragStop() {
+    isDragging = false;
+    sliderWrapper.classList.remove('dragging');
+}
